ad9dbe113d24c106eff6e6b911defe7cccb49772
ishumaurya1992 <43113212+ishumaurya1992@users.noreply.github.com>
2021-07-01 17:04:25 +0530

    Update PascalsTriangle.java

--- src/gsprep/miscellaneous/PascalsTriangle.java
+++ src/gsprep/miscellaneous/PascalsTriangle.java
@@ -48,14 +48,7 @@ public class PascalsTriangle {
                 }
             }
         }
-
-        for (int i = 0; i < arr.length ; i++) {
-            for (int j = 0; j < arr.length; j++) {
-             //   System.out.print(arr[i][j]+ " ");
-            }
-            System.out.println();
-        }
-
+     
         return arr[row][col];
 
     }
